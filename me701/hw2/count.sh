c47151a8182799e213c15dfb54a0a6c454cc27fa
John Boyington <mjcb@ksu.edu>
2017-09-05 17:00:49 -0500

    initialized trash script, added comments and formatting

--- me701/hw2/count.sh
+++ me701/hw2/count.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+# uses ls to list the files in the current directory, then wc (word count, I
+# assume) to count the number of new lines listed.
 numOfFilesReg="$(ls -1 | wc -l)"
 numOfFilesTot="$(ls -1a | wc -l)"
+
+# print something meaningful regarding the numbers calculated
 echo The number of regular files and folders in the current directory are $numOfFilesReg.
 echo The total number of files and folders in the current directory are $numOfFilesTot, including ., .., and other hidden items.
--- me701/hw2/temp.sh
+++ me701/hw2/temp.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# input temperature as cmd line flag
 inputTemp=$1
 
 # (inputTemp-32) * (5/9)
 cTemp=$(echo "scale=6;($inputTemp-32)*(5/9)" | bc)
+
 # cTemp + 273.15
 kTemp=$(echo "scale=6;($cTemp+273.15)" | bc)
 
+# output information
 echo The temperature \in celcius is $cTemp.
 echo The temperature \in kelvin is $kTemp.
