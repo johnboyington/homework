c8851d18bb6a21d1bc075c92b22a29e37870bde6
John Boyington <mjcb@ksu.edu>
2017-09-05 16:38:59 -0500

    added kelvin feature; script now requires flag

--- me701/hw2/temp.sh
+++ me701/hw2/temp.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 inputTemp=$1
-fTemp=212
 
-# inputTemp * (5/9)
-cTemp=$(echo "scale=6;($fTemp-32)*(5/9)" | bc)
+# (inputTemp-32) * (5/9)
+cTemp=$(echo "scale=6;($inputTemp-32)*(5/9)" | bc)
+# cTemp + 273.15
+kTemp=$(echo "scale=6;($cTemp+273.15)" | bc)
 
 echo The temperature \in celcius is $cTemp.
+echo The temperature \in kelvin is $kTemp.
