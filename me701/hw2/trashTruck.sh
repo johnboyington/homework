1d75fb2c1c43225c8218b341022606978461c3eb
John Boyington <mjcb@ksu.edu>
2017-09-05 17:16:51 -0500

    finished file removal script

--- me701/hw2/trashTruck.sh
+++ me701/hw2/trashTruck.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-touch ~/trash
+# create trash folder in home directory and output appropriate message
+(((mkdir ~/trash) 2> /dev/null) && echo trash folder created in home directory) || echo trash folder already exists in home directory
+
+# take in input filename
+fileToDelete=$1
+
+# move file to trash if it exist and output appropriate message
+(((mv $fileToDelete ~/trash) 2> /dev/null) && echo $fileToDelete moved to trash folder) || echo $fileToDelete does not exist in current directory
