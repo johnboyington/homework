58c61ac4e04c787b88a5f7f73422056d36e35acf
John Boyington <mjcb@ksu.edu>
2017-09-21 16:30:10 -0500

    added comments

--- me701/hw3/boyington_john_hw3.py
+++ me701/hw3/boyington_john_hw3.py
@@ -78,53 +78,67 @@ print(len(points) == 3 * 4 * 2)
 
 
 def decimal_to_binary(x, n):
+    # divide the input float into left and right of decimal
     strX = str(x)
     left, right = strX[:strX.index('.')], strX[strX.index('.'):]
 
     # for the left side
     leftLength = len(left)
     leftTotal = 0
+    # using the length 'left' as the starting point
     leftBin = np.ones(leftLength)
+
+    # find the number of binary digits required to represent the left of the decimal
     while leftTotal < int(left):
         leftTotal = np.array([e * (2**((ind + 1))) for ind, e in enumerate(leftBin[::-1])]).sum()
         leftBin = np.concatenate((np.array([1]), leftBin))
     finalLen = len(leftBin)
-
+    # reset to zero
     leftBin = np.zeros(finalLen)
-
+    # iteratively check if a 1 for the leading digit gives the number, or is too high
     for i in range(finalLen):
         leftBin[i] = 1
         summed = np.array([e * (2 ** (-(ind + 1) + finalLen)) for ind, e in enumerate(leftBin)]).sum()
         if summed > float(left):
             leftBin[i] = 0
     leftBinString = ''
+    # convert to string
     for e in leftBin.astype(int):
         leftBinString += '{}'.format(e)
 
     # for the right side of the decimal
     rightBin = np.zeros(n)
+    # iteratively check if a 1 for the leading digit gives the number, or is too high
     for i in range(n):
         rightBin[i] = 1
         summed = np.array([e * (2**(-(ind + 1))) for ind, e in enumerate(rightBin)]).sum()
         if summed > float(right):
             rightBin[i] = 0
     rightBinString = ''
+    # convert to string
     for e in rightBin.astype(int):
         rightBinString += '{}'.format(e)
     return leftBinString + '.'+rightBinString
 
-
+# check result with example given
 print(decimal_to_binary(123.625, 4) == '1111011.1010')
 
 
 def binary_to_decimal(i, f):
+    # convert input to string
     strI = str(i)
+    # divide into left and right of decimal
     left, right = np.array(list(strI[:strI.index('.')])).astype(float), np.array(list(strI[strI.index('.') + 1:])).astype(float)
+    # sum left and right side of decimals after multiplying 2**index
     leftTot = np.array([e * (2 ** (-(ind + 1) + len(left))) for ind, e in enumerate(left)]).sum()
     rightTot = np.array([e * (2**(-(ind + 1))) for ind, e in enumerate(right)]).sum()
     return leftTot + rightTot
 
+# check result with original input
 print(binary_to_decimal(1111011.1010, 4) == 123.625)
+
+# NOTE: it wasn't mentioned what the 'f' argument was in the problem statement
+#       so I didn't use it in my function.
 
 
 ###############################################################################
@@ -135,6 +149,7 @@ dec.getcontext().prec = 200
 
 
 def option1(a):
+    # option1 from the problem statement
     s = 0
     for i in range(len(a)):
         s += a[i]
@@ -142,6 +157,7 @@ def option1(a):
 
 
 def option2(a):
+    # option2 from the problem statement
     s = 0
     a = sorted(a)
     for i in range(len(a)):
@@ -150,6 +166,7 @@ def option2(a):
 
 
 def option3(a):
+    # option3 from the problem statement
     if len(a) <= 2:
         s = sum(a)
     else:
@@ -158,14 +175,18 @@ def option3(a):
 
 
 def optionDec(a):
+    # uses dec.Decimal to give the closest to the "perfect" solution
     s = 0
     for i in range(len(a)):
         s += dec.Decimal('{:.200}'.format(a[i]))
     return s
 
 
+# initialize parameters
 n = 200
 data = np.zeros((n, 5))
+
+# iteratively store the difference between each algorithm and the "perfect" answer
 for i in range(n):
     a = np.random.rand(i + 1)
 
@@ -186,6 +207,7 @@ for i in range(n):
     data[i, 4] = dec.Decimal(best) - dec.Decimal(opt3)
 
 
+# plot everythin on linear scale
 plt.figure(0)
 plt.plot(np.arange(n) + 1, abs(data[:, 0]), label='Regular Sum')
 plt.plot(np.arange(n) + 1, abs(data[:, 1]), label='Numpy Sum')
@@ -194,10 +216,10 @@ plt.plot(np.arange(n) + 1, abs((data[:, 3])), label='Option 2')
 plt.plot(np.arange(n) + 1, abs((data[:, 4])), label='Option 3')
 plt.xlabel('Number of Elements in Array')
 plt.ylabel('Difference from Most Accurate')
-plt.yscale('log')
 plt.legend()
 plt.savefig('errorComparison.png', dpi=400)
 
+# plot given algorithms on log scale
 plt.figure(1)
 plt.plot(np.arange(n) + 1, abs((data[:, 2])), label='Option 1')
 plt.plot(np.arange(n) + 1, abs((data[:, 3])), label='Option 2')
@@ -208,6 +230,7 @@ plt.yscale('log')
 plt.legend()
 plt.savefig('errorComparisonOptions.png', dpi=400)
 
+# plot built in and numpy sum functions on log scale
 plt.figure(2)
 plt.plot(np.arange(n) + 1, abs(data[:, 0]), label='Regular Sum')
 plt.plot(np.arange(n) + 1, abs(data[:, 1]), label='Numpy Sum')
@@ -217,7 +240,7 @@ plt.yscale('log')
 plt.legend()
 plt.savefig('errorComparisonBuiltIns.png', dpi=400)
 
-# check regular sum algorithm
+# initialize some check variables
 check1 = True
 check2 = True
 check3 = True
@@ -225,6 +248,8 @@ check4 = True
 check5 = True
 check6 = True
 
+# if datasets are identical, the check remains the same
+# this compares the built ins with the given algorithms
 for i in range(n):
     if not data[i, 0] == data[i, 2]:
         check1 = False
@@ -240,6 +265,7 @@ for i in range(n):
         check6 = False
 
 
+# print the resulting info from the checks
 print('Regular Sum is Identical to Option 1', check1)
 print('Regular Sum is Identical to Option 2', check2)
 print('Regular Sum is Identical to Option 3', check3)
@@ -247,3 +273,11 @@ print('Regular Sum is Identical to Option 3', check3)
 print('Numpy Sum is Identical to Option 1', check4)
 print('Numpy Sum is Identical to Option 2', check5)
 print('Numpy Sum is Identical to Option 3', check6)
+
+#   As seen, the most accurate for a list of random floats is option 3,
+# this is because it assures with each summation that the numbers being summed
+# will be closer to eachother and there's less chance to lose iniformation.
+#   With option 1, the next number is always summed to a total, which increases
+# as the number of elements increases. This means the larger it is, the more
+# likely one is to lose info with each calculation.
+#   Option 1 is the same as the python built in sum function.
