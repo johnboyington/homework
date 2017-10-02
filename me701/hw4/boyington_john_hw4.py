e0d7a682a7961a2ce698eb55627a7bff89f92d9a
johnboyington <mjcb@ksu.edu>
2017-10-02 14:37:41 -0500

    began working on p1

--- me701/hw4/boyington_john_hw4.py
+++ me701/hw4/boyington_john_hw4.py
@@ -1,8 +1,24 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Oct  2 13:37:46 2017
+###############################################################################
+#                                Homework 4
+###############################################################################
 
-@author: user
-"""
+import re
 
+
+###############################################################################
+#                                PROBLEM 1
+###############################################################################
+
+
+goodString = 'pit spot spate slap two respite '
+badString = 'pt Pot peat part '
+badList = badString.split()
+goodList = goodString.split()[:3] + ['slap two']  + goodString.split()[-1:]
+
+totString = goodString + badString
+
+expresso = r'p[a-z]+t'
+found = re.findall(expresso, totString)
+
+print(found == goodList)
+print(found == badList)
