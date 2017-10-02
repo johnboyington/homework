a637b834fe70ba5eed271693ce9f755431f83c4d
John Boyington <mjcb@ksu.edu>
2017-10-02 18:06:47 -0500

    worked on p3

--- me701/hw4/boyington_john_hw4.py
+++ me701/hw4/boyington_john_hw4.py
@@ -3,14 +3,17 @@
 ###############################################################################
 
 import re
+import numpy as np
+import scipy as sp
+import matplotlib.pyplot as plt
 
 # TODO: comment everything
+# TODO: talk to Roberts about p3
 
 
 ###############################################################################
 #                                PROBLEM 1
 ###############################################################################
-
 
 goodString = 'pit spot spate slap two respite '
 badString = 'pt Pot peat part '
@@ -24,6 +27,7 @@ p = re.compile(s)
 found = p.findall(totString)
 
 print(sorted(found) == sorted(goodList))
+
 
 ###############################################################################
 #                                PROBLEM 2
@@ -61,3 +65,78 @@ checkDict = {18: {'energy': [0.0000E+00, 3.0000E-01, 2.0000E+02],
                   'sigma': [0.0000, 0.3778, 0.0003]}}
 
 print(d == checkDict)
+
+
+###############################################################################
+#                                PROBLEM 3
+###############################################################################
+
+# hard code bonner sphere data
+sphDia = np.array([0.0, 2.0, 3.0, 5.0, 8.0, 10.0, 12.0])
+resData = np.array([141585, 102435, 76796, 38056, 13923, 8091, 4834])
+
+M = np.array([sphDia, np.ones(len(sphDia))]).T
+
+z = np.linalg.lstsq(M, resData)[0]
+y_approx = M.dot(z)
+
+
+def fun(params, x, y):
+    a, b = params
+    y_approx = a * sp.exp(-x * b)
+    err = y - y_approx
+    return sum(abs(err))
+
+
+sol = sp.optimize.minimize(fun, x0=[160000, 0.1], args=(sphDia, resData))
+a2, b2 = sol.x
+xFine = np.linspace(0, 12, 100)
+y_approx2 = a2 * sp.exp(-xFine * b2)
+
+
+plt.figure(0)
+plt.plot(sphDia, resData, 'ro', label='Bonner Sphere Response Data')
+plt.plot(xFine, y_approx2, 'g:', label='Exponential Fit')
+plt.plot(sphDia, y_approx, 'b:', label='Linear Least Squares Fit')
+plt.xlabel('Sphere Diameter $cm$')
+plt.ylabel('Response $counts$')
+plt.legend()
+
+
+###############################################################################
+#                                PROBLEM 4
+###############################################################################
+
+# 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
