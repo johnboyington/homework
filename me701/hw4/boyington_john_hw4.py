875d84d1977e5941a1b240c402869937375eeb11
John Boyington <mjcb@ksu.edu>
2017-10-04 11:19:52 -0500

    hw changes

--- me701/hw4/boyington_john_hw4.py
+++ me701/hw4/boyington_john_hw4.py
@@ -6,9 +6,11 @@ import re
 import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
+from scipy.integrate import odeint
 
 # TODO: comment everything
 # TODO: talk to Roberts about p3
+# TODO: mention 0.5 norm, etc.
 
 
 ###############################################################################
@@ -75,50 +77,147 @@ print(d == checkDict)
 sphDia = np.array([0.0, 2.0, 3.0, 5.0, 8.0, 10.0, 12.0])
 resData = np.array([141585, 102435, 76796, 38056, 13923, 8091, 4834])
 
-M = np.array([sphDia, np.ones(len(sphDia))]).T
 
-z = np.linalg.lstsq(M, resData)[0]
-y_approx = M.dot(z)
-
-
-def fun(params, x, y):
+def fun(params, x, y, norm):
     a, b = params
     y_approx = a * sp.exp(-x * b)
-    err = y - y_approx
-    return sum(abs(err))
+    return np.linalg.norm(y - y_approx, ord=norm)
 
 
-sol = sp.optimize.minimize(fun, x0=[160000, 0.1], args=(sphDia, resData))
-a2, b2 = sol.x
+sol = sp.optimize.minimize(fun, x0=[160000, 0.01], args=(sphDia, resData, 2))
+a, b, = sol.x
+sol2 = sp.optimize.minimize(fun, x0=[160000, 0.1], args=(sphDia, resData, np.inf))
+a2, b2 = sol2.x
 xFine = np.linspace(0, 12, 100)
+y_approx1 = a * sp.exp(-xFine * b)
 y_approx2 = a2 * sp.exp(-xFine * b2)
 
 
 plt.figure(0)
 plt.plot(sphDia, resData, 'ro', label='Bonner Sphere Response Data')
-plt.plot(xFine, y_approx2, 'g:', label='Exponential Fit')
-plt.plot(sphDia, y_approx, 'b:', label='Linear Least Squares Fit')
+plt.plot(xFine, y_approx1, 'b:', label='Least-Squares Fit')
+plt.plot(xFine, y_approx2, 'g:', label='Minimax Fit')
 plt.xlabel('Sphere Diameter $cm$')
 plt.ylabel('Response $counts$')
 plt.legend()
+plt.show()
+plt.close()
 
 
 ###############################################################################
 #                                PROBLEM 4
 ###############################################################################
 
+#from scipy.integrate import odeint
+#def rhs(y, t, a):
+#    return -a*y[0]
+#times = sp.linspace(0, 10)
+#sol = odeint(rhs, [1], times, args=(2,)) # notice that (2,) is needed to make it appears as a tuple.
+#sol = sol.reshape((len(times))) # returns an array of array (one array per time step with each unknown)
+#plt.plot(times, sol)
+#plt.xlabel('t')
+#plt.ylabel('y(t)')
+#plt.show()
+
+
+times = sp.linspace(0, 10, 100)
+
 # 1
+def rhs1(y, t):
+    return y + 1
+
+sol1 = odeint(rhs1, [1], times)
+
+plt.figure(1)
+plt.plot(times, sol1)
+plt.title('Equation 1')
+plt.xlabel('t')
+plt.ylabel('y(t)')
+plt.show()
+plt.close()
 
 
 
+#def rhs(u, t, a, b): # using u.  remember, function argument names are arbitrary
+#    y, z = u # unpack
+#    dydt = -a*y
+#    dzdt = -b*z + a*y
+#    return dydt, dzdt
+#times = sp.linspace(0, 10)
+#sol = odeint(rhs, [1, 0], times, args=(1, 1)) # notice that (2,) is needed to make it appears as a tuple.
+#sol = sol.T
+#plt.plot(times, sol[0], 'k-', label='y(t)')
+#plt.plot(times, sol[1], 'r--', label='z(t)')
+#plt.xlabel('t')
+#plt.legend()
+#plt.show()
+
+# 2
+#def rhs2(args, t):
+#    y, z, a = args
+#    return z, a, y - z
+#
+#sol2 = odeint(rhs2, [1, 1, 1], times)
+#
+#
+#
+#plt.figure(2)
+#plt.plot(times, sol2)
+#plt.title('Equation 2')
+#plt.xlabel('t')
+#plt.ylabel('y(t)')
+#plt.show()
+#plt.close()
+
+
+# 3
+def rhs3(args, t, a, b):
+    y, z = args
+    dydt = (a * y) + 1
+    dzdt = (b * z) + (a * y)
+    return dydt, dzdt
+
+sol3 = odeint(rhs3, [1, 1], times, args=(1000, 0.0001)).T
+
+plt.figure(3)
+plt.plot(times, sol3[0], label="y'")
+plt.plot(times, sol3[1], label="z'")
+plt.title('Equation 3')
+plt.xlabel('t')
+plt.ylabel('y(t)')
+#plt.yscale('log')
+plt.legend()
+plt.show()
+plt.close()
 
 
 
+# 4
+def rhs4(y, t):
+    return y**2 + 1
+times = np.linspace(0, 10, 100000)
+sol4 = odeint(rhs1, [1], times)
+
+myY = [1]
 
 
+for i in range(len(times)-1):
+    step = (myY[i]**2 + 1) * (times[i + 1] - times[i]) + myY[i]
+    myY.append(step)
 
 
-
+plt.figure(4)
+plt.plot(times, sol4, label='Using odeint')
+plt.plot(times, myY, label="Euler's Method")
+plt.title('Equation 4')
+plt.xlabel('t')
+plt.ylabel('y(t)')
+plt.yscale('log')
+plt.xlim(0, 10)
+plt.ylim(10**0, 10**5)
+plt.legend()
+plt.show()
+plt.close()
 
 
 
