3feaf1e5c2d6a2aa540196bcb4b2159b03c5cd90
John Boyington <mjcb@ksu.edu>
2017-10-04 16:27:16 -0500

    done w/ problems 1 and 2

--- me760/hw2/boyington_john_hw2.py
+++ me760/hw2/boyington_john_hw2.py
@@ -1,18 +1,26 @@
 import numpy as np
 
 
+# --------------------------------------------------problem 1
+print('\nProblem 1 \n')
 
+C = np.array([[4,  6,  2],
+              [6,  0,  3],
+              [2,  3, -1]])
 
-
+print(C.dot(C))
 
 
 # --------------------------------------------------problem 2
+print('\nProblem 2 \n')
+
 A = np.array([[1,  1, -1],
               [0,  8,  6],
-              [-2, 4,  6]])
+              [-2, 4,  -6]])
 
 b = np.array([9, -6, 40])
 
 sol2a = np.linalg.solve(A, b)
-print(sol2a)
-print(np.allclose(np.dot(A, sol2a), b))
+print('Solution =', sol2a)
+print('Det = ', np.linalg.det(A))
+print('Inverse = \n', np.linalg.inv(A))
