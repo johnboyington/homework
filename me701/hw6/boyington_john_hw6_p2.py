6540a3976299ca43170ec2349104e7af4197003a
John Boyington <mjcb@ksu.edu>
2017-10-25 09:48:18 -0500

    finished p2

--- me701/hw6/boyington_john_hw6_p2.py
+++ me701/hw6/boyington_john_hw6_p2.py
@@ -23,7 +23,28 @@ def getTemp(alpha, L=1, tMax=0.1):
     for i in range(Nt):
         temp[1:-1] += dt * alpha / dx ** 2 * (temp[0:-2] - 2 * temp[1:-1] + temp[2:])
     return temp, np.linspace(0, L, Nx)
-    
-T, x = getTemp(alpha)
-plt.plot(x, T)
-plt.show()
+
+
+
+
+
+def live_animation():
+    # number particles
+    n = 1000
+    # initialize particle positions
+    # set up live
+    plt.ion()
+    plt.show()
+    alphas = np.linspace(0, 1, 100)
+    for a in alphas:
+        plt.figure(1)
+        plt.clf()
+        T, x = getTemp(a)
+        plt.plot(x, T)
+        plt.title('alpha = {}'.format(a))
+        plt.axis([0, 1, 0, 1])
+        plt.draw()
+        plt.pause(0.02)
+   
+
+live_animation()
