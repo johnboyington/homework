7d18d2ca95b30e4e53fc50f82f0f09aedba0b3ff
John Boyington <mjcb@ksu.edu>
2017-11-27 15:29:04 -0600

    changed interpolate to fortran

---
+++ me701/hw8/fortran/interpolate.f90
@@ -0,0 +1,48 @@
+
+
+double precision function interpolate(x_new, x, y, n, order)
+    double precision, intent(in) :: x_new
+    double precision, dimension(:), intent(in) :: x, y
+    integer, intent(in) :: n, order
+    integer :: l=0, i=0, j=0, s=0, p=1
+    integer ind(order+1)
+    
+    ! find points directly to left of x_new
+    do i=0, n
+        if(x_new .ge. x[i]) then
+            l = i
+            EXIT
+        end if
+    end do
+    
+    ! populate interpolation points
+    do j=l, order+l+1
+        ind(j-l) = j-((order+1)/2)
+    end do
+    
+    ! shift interpolation points into existence if negative
+    if (ind(0) .le. 0) then
+        do i=0, order+1
+            ind(i) += 0 - ind(0)
+        end do
+    end if
+    
+    ! shift interpolation points into existence if higher than n
+    if (ind(order) .le. n-1) then
+        do i=0, order+1
+            ind(i) += (n - 1) - ind(order)
+        end do
+    end if
+    
+    ! calculate Lagrange polynomials
+    do i=0, order+1
+        for(j=0; j<=order; j++)
+        do j=0, order+1
+            if(j .ne. i) then
+                p *= ((x_new - x(ind(j))) / (x(ind(i)) - x(ind(j))))
+            end if
+        end do
+        s += (p * y(ind(i)));
+        p = 1;
+    end do
+end function interpolate
--- me701/hw8/interpolate.hh
+++
@@ -1,2 +0,0 @@
-// interpolate
-double interpolate(double x_new, double *x, double *y, int n, int order);
