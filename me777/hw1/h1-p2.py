8f6aa037da6b35b9ce8be733e375fd1fc952035e
johnboyington <mjcb@ksu.edu>
2018-02-05 10:54:50 -0600

    finished problems 1-3

--- me777/hw1/h1-p2.py
+++ me777/hw1/h1-p2.py
@@ -2,7 +2,6 @@
 me777 homework 1 problem 2; book problem 1-3
 '''
 import numpy as np
-from numpy.random import rand
 import matplotlib.pyplot as plt
 
 
@@ -15,32 +14,20 @@ x_l = -4
 x_h = 4
 x_diff = x_h - x_l
 
-y_l = 0
-y_h = 1
-y_diff = y_h - y_l
-
-under = 0
-total = 0
-
-#for i in range(10000):
-#    pseudo = np.append(pseudo, rand())
-
-
 x_full = np.linspace(-4, 4, 100)
 fig = plt.figure(0)
 ax = fig.add_subplot(111)
 ax.plot(x_full, u(x_full))
 
-for i in range(len(pseudo))[::2]:
-    x, y = pseudo[i], pseudo[i+1]
-    x = x * x_diff + x_l
-    y = y * y_diff + y_l
+total = 0
+s = 0
+for x in pseudo:
+    x = x_l + (x_diff * x)
+    y = u(x)
+    s += y
     ax.plot(x, y, 'ko', ms=3)
-    y_true = u(x)
-    if y < y_true:
-        under += 1
     total += 1
 
-integral = (under / total) * (x_diff * y_diff)
+integral = (s / total) * (x_diff)
 
 print('The value of the integral is:  {}'.format(integral))
--- me777/hw1/h1-p3.py
+++ me777/hw1/h1-p3.py
@@ -14,9 +14,6 @@ x_l = 0
 x_h = np.pi
 x_diff = x_h - x_l
 
-y_l = 0
-y_h = 0.175
-y_diff = y_h - y_l
 
 under = 0
 total = 0
@@ -26,21 +23,19 @@ fig = plt.figure(0)
 ax = fig.add_subplot(111)
 ax.plot(x_full, pcut(x_full, 1, 2))
 ax.set_xlim(x_l, x_h)
-ax.set_ylim(y_l, y_h)
 
-for i in range(5000):
-    x, y = rand(), rand()
-    x = x * x_diff + x_l
-    y = y * y_diff + y_l
-    y_true = pcut(x, 1, 2)
-    if y < y_true:
-        c = 'r'
-        under += 1
-    else:
-        c = 'indigo'
-    ax.plot(x, y, 'ko', color=c, ms=0.5)
+s = 0
+for i in range(2000):
+    x = rand()
+    x = x_l + (x_diff * x)
+    y = pcut(x, 1, 2)
+    s += y
+    ax.plot(x, y, 'ko', color='k', ms=0.5)
     total += 1
 
-integral = (under / total) * (x_diff * y_diff)
+integral = (s / total) * (x_diff)
+pcut_true = 1 / np.pi
+diff = integral - pcut_true
 
 print('The value of the integral is:  {}'.format(integral))
+print('The difference from the true value is:  {}'.format(diff))
