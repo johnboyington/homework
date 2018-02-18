7298cf834e9fc48b850365bb3e0435ecb995e655
John Boyington <mjcb@ksu.edu>
2018-02-18 17:30:47 -0600

    finished problem 4

---
+++ me777/hw2/hw2-p4.py
@@ -0,0 +1,20 @@
+'''
+me777 hw2 problem 4
+'''
+
+
+def pm(x_i, a, m):
+    return (a * x_i) % m
+
+
+a = 16807
+m = (2**31) - 1
+x_0 = 1
+
+x_n = x_0
+i = 1
+while i < 10001:
+    x_n = pm(x_n, a, m)
+    i += 1
+
+print('x_{} = {}'.format(i, x_n))
