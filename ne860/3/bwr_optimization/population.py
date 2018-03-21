2394d56d1c8e12407b65f09ceb0f0d7c3309bcbb
John Boyington <mjcb@ksu.edu>
2018-03-21 17:10:09 -0500

    commented out modules that eigendoit node didnt have

--- ne860/3/bwr_optimization/population.py
+++ ne860/3/bwr_optimization/population.py
@@ -5,8 +5,7 @@ import numpy as np
 from operations import ops
 # import matplotlib.pyplot as plt
 import copy
-# from mpi4py import MPI
-# comm = MPI.COMM_WORLD
+d# comm = MPI.COMM_WORLD
 # rank = comm.Get_rank()
 # size = comm.Get_size()
 
