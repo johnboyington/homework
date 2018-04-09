22f09ddd891f388134080aad53f2af5b74a47b29
John Boyington <mjcb@ksu.edu>
2018-04-09 11:19:46 -0500

    finished ne860 hw3; started me777 hw5

--- ne860/3/bwr_probe/bwr_probe.py
+++ ne860/3/bwr_probe/bwr_probe.py
@@ -3,11 +3,22 @@ from material import material_library, rand_material, mat_id
 from numpy.random import randint
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import rc, rcParams
+
+# nice plots
+rc('font', **{'family': 'serif'})
+rcParams['xtick.direction'] = 'out'
+rcParams['ytick.direction'] = 'out'
+rcParams['xtick.labelsize'] = 12
+rcParams['ytick.labelsize'] = 12
+rcParams['lines.linewidth'] = 1.85
+rcParams['axes.labelsize'] = 15
+rcParams.update({'figure.autolayout': True})
 
 
 class Probe(object):
     def __init__(self):
-        self.individuals(False)
+        self.individuals(True)
         # self.test()
 
     def test(self):
@@ -43,25 +54,46 @@ class Probe(object):
 
     def individuals(self, ran):
         fuel = \
-        [(2,4),
-         (2.2,0), (2.5,0), 
-         (2.5,0), (3.0,0), (3.5,0), 
-         (3.0,2), (3.0,0), (3.7,0), (3.0,0), 
-         (3.0,2), (3.0,0), (3.7,0), (3.0,0), (3.0,0), 
-         (3.0,2), (3.0,0), (3.0,0), (2.0,2), (2.0,2), (3.0,0), 
-         (2.5,0), (3.0,0), (3.0,0), (2.0,2), (2.0,2), (3.0,0), (4.0,0), 
-         (2.5,0), (3.0,0), (3.0,0), (3.0,0), (3.0,0), (3.4,0), (4.2,0), (4.0,0), 
-         (2.0,0), (3.0,0), (3.0,0), (3.0,0), (3.0,0), (3.0,0), (3.0,0), (3.0,0), (3.0,0), 
-         (2.0,0), (2.0,0), (2.5,2), (2.5,2), (2.5,2), (2.5,2), (3.0,0), (2.5,0), (2.0,0), (2,4)]
+        [(2.0,0),
+         (2.2,0), (3.0,0), 
+         (2.5,0), (3.8,0), (4.0,0), 
+         (3.2,0), (4.0,0), (4.5,0), (4.0,0), 
+         (3.2,0), (4.0,0), (4.0,0), (3.0,0), (3.5,0), 
+         (3.2,0), (4.5,0), (4.0,0), (2.0,0), (2.0,0), (3.5,0), 
+         (3.2,0), (4.5,0), (4.0,0), (2.0,0), (2.0,0), (3.5,0), (4.0,0), 
+         (3.0,0), (3.5,0), (4.0,0), (4.0,0), (4.0,0), (4.2,0), (4.5,0), (4.0,0), 
+         (2.0,10), (3.2,2), (4.0,0), (4.0,0), (4.0,0), (4.0,0), (4.0,0), (4.0,0), (3.5,0), 
+         (2.0,10), (2.0,10), (3.0,0), (3.2,0), (3.2,0), (2.8,0), (3.0,0), (2.5,0), (2.0,0), (2.0,0)]
         
         c = []
         for f in fuel:
             c.append(mat_id(*f))
-        indiv = Core(206, c)
+        indiv = Core(235, c)
         indiv.write()
         if ran:
             indiv.extract()
             self.plot_individual(indiv)
+        
+        gad = np.zeros((10,10))
+        en = np.zeros((10,10))
+        indices = np.tril_indices(55)
+        for i, f in enumerate(fuel):
+            gad[indices[0][i], indices[1][i]] = f[1]
+            en[indices[0][i], indices[1][i]] = f[0]
+            print(f[1])
+        fig = plt.figure(10)
+        ax = fig.add_subplot(111)
+        image = ax.imshow(gad, cmap='YlOrBr')
+        fig.colorbar(image)
+        fig.savefig('gad.png', dpi=300)
+
+        # enrichment
+        fig = plt.figure(11)
+        ax = fig.add_subplot(111)
+        image = ax.imshow(en, cmap='Reds')
+        fig.colorbar(image)
+        fig.savefig('enrichment.png', dpi=300)
+        
 
     def plot_individual(self, ind):
         print('\n\n')
@@ -72,6 +104,7 @@ class Probe(object):
         # plot core map
         fig = plt.figure(1, figsize=(10, 8))
         ax = fig.add_subplot(224)
+        ax.set_xlabel('EOL pppf')
         extreme = max(abs(1-ind.coremap.min()), abs(1-ind.coremap.max()))
         image = ax.imshow(ind.coremap, cmap='bwr', vmin=1-extreme, vmax=1+extreme)
         fig.colorbar(image)
@@ -98,5 +131,13 @@ class Probe(object):
         ax.set_ylabel('Burnup')
         ax.plot(ind.burnup)
 
+        # save
+        fig.savefig('best.png', dpi=300)
+        
+
 
 Probe()
+
+'''
+232
+'''
