700a946a21518ca5c25e1a6237e7dbc07b95e8b5
John Boyington <mjcb@ksu.edu>
2018-05-08 15:05:32 -0500

    ran huge monte carlo run

--- me777/project/sandwich.py
+++ me777/project/sandwich.py
@@ -4,7 +4,7 @@ from sandii import iterate
 from scipy.stats import norm
 from numpy.random import rand
 import matplotlib.pyplot as plt
-
+from time import time
 from matplotlib import rc, rcParams
 from nebp_response_functions import response_matrix
 from experimental_data import unfiltered1
@@ -55,17 +55,37 @@ y2 = np.zeros(len(f_i))
 #                                iterative comparison
 ##############################################################################
 
-n = 10
+n = 2500
+y = np.zeros((n, len(f_i)))
+y2 = np.zeros((n, len(f_i)))
+
+'''
 fig = plt.figure(2)
 ax = fig.add_subplot(111)
+'''
+ran = True
+t_i = time()
+if not ran:
+    for i in range(n):
+        sol = iterate(f_i, sample(N, sig), sig, R)
+        sol_spec = Spectrum(nebp.edges, sol)
+        y[i] = (sol)/n
+        y2[i] = (sol**2)/n
+        # ax.plot(sol_spec.step_x, sol_spec.step_y)
 
-for i in range(n):
-    sol = iterate(f_i, sample(N, sig), sig, R)
-    sol_spec = Spectrum(nebp.edges, sol)
-    y += (sol)/n
-    y2 += (sol**2)/n
-    ax.plot(sol_spec.step_x, sol_spec.step_y)
+    np.save('y.npy', y)
+    np.save('y2.npy', y2)
 
+t_f = time() - t_i
+print('\ntime:  {}\n'.format(t_f))
+y = np.load('y.npy')
+y2 = np.load('y2.npy')
+
+y_sum = np.sum(y, axis=0)
+y2_sum = np.sum(y2, axis=0)
+
+
+'''
 ax.set_xscale('log')
 ax.set_yscale('log')
 ax.set_xlabel('Energy $MeV$')
@@ -75,15 +95,34 @@ plt.legend()
 ax.spines['top'].set_visible(False)
 ax.spines['right'].set_visible(False)
 ax.legend(frameon=False)
+'''
 
 
-error = np.sqrt((n / (n - 1)) * (y2 - y**2))
-solution = Spectrum(nebp.edges, y, error)
+###############################################################################
+#                                sandwich comparison
+###############################################################################
+N = np.matrix(N)
+C_y = (1 / (len(N.T) - 1)) * (N.T * N)
+C_x = np.linalg.pinv(R) * C_y * np.linalg.pinv(R.T)
+sandwich_error = np.sqrt(C_x.diagonal() * (len(f_i) - 1))
+
+###############################################################################
+#                           individual bin comparison
+###############################################################################
+
+group = 20
+plt.plot(y[:, group] * n, 'ko', markersize=0.3)
+plt.plot([0, n - 1], [y_sum[group], y_sum[group]], color='indigo')
 
 
 ###############################################################################
 #                                final comparison
 ###############################################################################
+
+error = np.sqrt((n / (n - 1)) * (y2_sum - y_sum**2))
+solution = Spectrum(nebp.edges, y_sum, error)
+# error = sandwich_error
+
 fig = plt.figure(0)
 ax = fig.add_subplot(111)
 ax.plot(nebp.step_x, nebp.step_y, color='k', label='Default Spectrum', linewidth=0.7,)
@@ -99,4 +138,3 @@ ax.spines['top'].set_visible(False)
 ax.spines['right'].set_visible(False)
 ax.legend(frameon=False)
 plt.savefig('comparison.png', dpi=300)
-    
